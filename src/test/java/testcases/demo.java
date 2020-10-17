9e4a3d96feb3c2f6e85152292c8bdc151d98e7b6
hemanmehta <heman.mehta07@gmail.com>
2020-10-17 16:16:32 +0530

    Final commit by American Architect-2

---
+++ src/test/java/testcases/demo.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Demo Class Added by American Timezone Architect-2");
+
+	}
+
+}
